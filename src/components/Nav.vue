1d40e40759b4531d88c9c2e2216f19d9ea985e03
relara <relvira_7232@stu.southtexascollege.edu>
2022-02-22 18:55:23 -0600

    local changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,19 @@
 <template>
-<div class="bg-green-900 text-green-200">
-   <div class="container mx-auto flex items-center justify-between">
-    <h1 class="tracking-tighter text-3xl font-thin">Basic<span class="font-normal">Router</span>
+<div class="text-indigo-200 bg-indigo-900">
+   <div class="container flex items-center justify-between mx-auto">
+    <h1 class="text-3xl font-thin tracking-tighter">Vue<span class="font-normal">FakeAuth</span>
     </h1>
     <nav>
         <ul class="flex space-x-4">
             <router-link to="/">
-            <li class="py-8 px-4 hover: cursor-pointer hover:bg-green-500 hover:text-green-800">Home
+            <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-800">Home
             </li>
             </router-link>
             <router-link :to="{ name: 'About' }">
-               <li class="py-8 px-4 hover: cursor-pointer hover:bg-green-500 hover:text-green-80">About</li> 
+               <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-80">About</li> 
             </router-link>
             <router-link to="/hello">
-              <li class="py-8 px-4 hover: cursor-pointer hover:bg-green-500 hover:text-green-80">Hello</li>  
+              <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-80">Hello</li>  
             </router-link>
             
         </ul>
