e4e2125a1e91f2f535a3045f3095059f20c20727
relara <relvira_7232@stu.southtexascollege.edu>
2022-02-24 18:17:28 -0600

    Menu Login Logic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,3 +1,8 @@
+<script>
+import useAuth from "../composable/useAuth";
+const { isAuthenticated, logout } = useAuth();
+</script>
+
 <template>
 <div class="text-indigo-200 bg-indigo-900">
    <div class="container flex items-center justify-between mx-auto">
@@ -12,12 +17,16 @@
             <router-link :to="{ name: 'About' }">
                <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-80">About</li> 
             </router-link>
-            <router-link to="/login">
+            <router-link v-if="!isAuthenticated" to="/login">
               <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-80">Login</li>  
             </router-link>
-            <router-link :to="{ name: 'Secret' }">
+               
+            <router-link v-else :to="{ name: 'Secret' }">
               <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-80">Secret</li>  
             </router-link>
+            <button v-else @click="logout">
+                <li class="px-4 py-8 cursor-pointer hover: hover:bg-indigo-500 hover:text-indigo-80">Logout</li> 
+              </button> 
         </ul>
     </nav>
     </div> 
---
+++ src/composable/useAuth.js
@@ -0,0 +1,23 @@
+import { ref } from "vue";
+
+const isAuthenticated = ref(false);
+
+const user = ref("");
+
+const usersFromDB = [{ username: "admin", password: "admin", name: "Administrator" }, { username: "rlara", password: "rlara", name: "Raymundo" }];
+
+const useAuth = () => {
+    const login = (username, password) => {
+        const user = usersFromDB.find((user) => user.username === username && user.password);
+        if (user) { isAuthenticated.value = true;
+            user.value = user.name;
+        };
+    };
+    const logout = () => {
+        isAuthenticated.value = false;
+        user.value = "";
+    };
+    return { isAuthenticated, login, logout, user };
+};
+
+export default useAuth;
